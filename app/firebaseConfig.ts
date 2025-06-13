a8798fea850c92f9cc35ee178f582025d8926612
Resa Budi Prasetyo <120000539+resasa1@users.noreply.github.com>
2025-06-13 19:52:40 +0700

    firebaseConfig add

---
+++ app/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// app/firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "APIKEY_MU",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
